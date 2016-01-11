ecff5bd0f53743d4755b491ae0ebc73bdf9c370d
sarahkami <sa.ganter@web.de>
2016-01-11 16:29:47 +0100

    Finish user edit, update, index and destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ActiveRecord::Base
 attr_accessor :remember_token
 before_save { self.email = email.downcase }
-validates :name, presence: true, length: { maximum: 50 } 
+validates :name, presence: true, length: { maximum: 50 }
 VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 validates :email, presence: true, length: { maximum: 255 }
 validates :email, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false}
-validates :password, presence: true, length: { minimum: 6 }
+validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 has_secure_password
 
   def self.digest(string)
